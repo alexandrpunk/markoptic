759ff3f1b270d07e81aae790d9a4561cb297c297
alexandrpunk <alexandrpunk@gmail.com>
2015-08-08 14:10:42 -0600

    cambios al formulario de recbo

--- recibo.php
+++ recibo.php
@@ -23,20 +23,42 @@
                                 <label for="nombre">Nombre Completo</label>
                                 <input type="text" class="form-control" id="nombre" placeholder="Nombre completo">
                               </div>
+                            
                               <div class="form-group">
                                 <label for="email">Correo Electronico</label>
                                 <input type="email" class="form-control" id="email" placeholder="correo electronico">
                               </div>
-                            <div class="col-md-6"></div>
-                         
+                            
+                            <div class="form-group">
+                                <label for="direccion">Direccion</label>
+                                <textarea id="direccion" class="form-control" placeholder="Ingrese su direccion de facturacion"></textarea>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="rfc">R.F.C.</label>
+                                <input id="rfc" class="form-control" placeholder="Registro Federal de Contribuyente">
+                            </div>
+                            
+                            <div class="col-md-6 col-sm-6 zero">
+                                <div class="form-group">
                                 <label for="monto">Monto del Donativo</label>
                                 <div class="input-group">
                                   <div class="input-group-addon">$</div>
                                   <input type="number" class="form-control" id="monto" placeholder="monto">
                                 </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6 zero col-sm-6">
+                                <div class="form-group">
                                  
-                                 <label for="autorizacion">No. de Autorizacion</label>
-                                <input type="number" class="form-control" id="autorizacion" placeholder="Numero de autorizacion">
+                                 <label for="referencia">No. de Referencia</label>
+                                <input type="number" class="form-control" id="referencia" placeholder="Numero de autorizacion">
+                                </div>
+                            </div>
+                         
+
+
                          
                               <button type="submit" class="btn btn-primary">Solicitar</button>
                         </form>
