bdba2ad0dfdcc907208879b776f2f62a2e2e6fa2
alexandrpunk <alexandrpunk@gmail.com>
2015-09-28 09:21:08 -0600

    modificaciones al archivo solicitud.php

--- solicitud.php
+++ solicitud.php
@@ -22,7 +22,7 @@ $paises = Paises();
                 <?php require 'mod/menu.php';?>
                 
                   <div class="panel panel-default panel-mark animated fadeIn">
-                    <div class="panel-heading panel-heading-mark" id="rosado">Solicitud de Prótesis/Colchon</div>
+                    <div class="panel-heading panel-heading-mark" id="rosado">Solicitud de Prótesis/Colchón</div>
                     <div class="panel-body panel-body-mark">    
                         <form method="POST" action="inc/solicitud.post.php" enctype="multipart/form-data" onsubmit="return funcionEnviar()">
 
@@ -31,17 +31,17 @@ $paises = Paises();
                             <h3 style="margin:20px 5%;" class="text-center decor-donar"><span class="decor-span">¿CON QUE TE PODEMOS AYUDAR?</span></h3>
                             <div class="form-horizontal">
                                 <div class="form-group">
-                                    <label for="peticion" class="col-sm-3 control-label">Peticion</label>
+                                    <label for="peticion" class="col-sm-3 control-label">Petición</label>
                                     <div class="col-sm-8">
                                         <select class="form-control" name="peticion" id="peticion">
                                             <option value="">Seleccioné Una Opción</option>
-                                            <option value="Protesis">Protesis</option>
-                                            <option value="Colchon Antiescaras">Colchon Antiescaras</option>
+                                            <option value="Protesis">Prótesis</option>
+                                            <option value="Colchon Antiescaras">Colchón Anti-escaras</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="form-group hidden" id="form-solicitud-seccion1-1">
-                                    <label for="opcion_protesis" class="col-sm-3 control-label">Opciones de Protesis</label>
+                                    <label for="opcion_protesis" class="col-sm-3 control-label">Opciones de Prótesis</label>
                                     <div class="col-sm-8">
                                         <select class="form-control" name="opcion_protesis" id="opcion_protesis">
                                             <option value="">Seleccioné Una Opción</option>
@@ -105,9 +105,9 @@ $paises = Paises();
 
                                 <div class="col-sm-4">
                                     <div class="form-group">
-                                        <label for="pais">Pais</label>
+                                        <label for="pais">País</label>
                                         <select class="form-control" name="pais" id="pais">
-                                            <option value="">Seleccioné Su Pais</option>
+                                            <option value="">Seleccioné Su País</option>
                                             <?php
                                             foreach ($paises as $p) {
                                             ?>
@@ -140,8 +140,8 @@ $paises = Paises();
 
                                 <div class="col-sm-12">
                                     <div class="form-group">
-                                        <label for="direccion">Calle y Numero</label>
-                                        <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Calle y Numero">
+                                        <label for="direccion">Calle y Número</label>
+                                        <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Calle y Número">
                                     </div>
                                 </div>
 
@@ -154,14 +154,14 @@ $paises = Paises();
 
                                 <div class="col-sm-4">
                                     <div class="form-group">
-                                        <label for="cp">Codigo Postal</label>
+                                        <label for="cp">Código Postal</label>
                                         <input type="text" class="form-control" id="cp" name="cp" placeholder="Codigo Postal">
                                     </div>
                                 </div>
 
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="telefono">Telefono</label>
+                                        <label for="telefono">Teléfono</label>
                                         <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono">
                                     </div>
                                 </div>
@@ -251,9 +251,9 @@ $paises = Paises();
 
                                 <div class="col-sm-4">
                                     <div class="form-group">
-                                        <label for="pais">Pais</label>
+                                        <label for="pais">País</label>
                                         <select class="form-control" name="t_pais" id="t_pais">
-                                            <option value="">Seleccioné Su Pais</option>
+                                            <option value="">Seleccioné Su País</option>
                                             <?php
                                             foreach ($paises as $p) {
                                             ?>
@@ -286,8 +286,8 @@ $paises = Paises();
 
                                 <div class="col-sm-12">
                                     <div class="form-group">
-                                        <label for="direccion">Calle y Numero</label>
-                                        <input type="text" class="form-control" id="t_direccion" name="t_direccion" placeholder="Calle y Numero">
+                                        <label for="direccion">Calle y Número</label>
+                                        <input type="text" class="form-control" id="t_direccion" name="t_direccion" placeholder="Calle y Número">
                                     </div>
                                 </div>
 
@@ -300,14 +300,14 @@ $paises = Paises();
 
                                 <div class="col-sm-4">
                                     <div class="form-group">
-                                        <label for="cp">Codigo Postal</label>
+                                        <label for="cp">Código Postal</label>
                                         <input type="text" class="form-control" id="t_cp" name="t_cp" placeholder="Codigo Postal">
                                     </div>
                                 </div>
 
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="telefono">Telefono</label>
+                                        <label for="telefono">Teléfono</label>
                                         <input type="text" class="form-control" id="t_telefono" name="t_telefono" placeholder="Telefono">
                                     </div>
                                 </div>
@@ -344,7 +344,7 @@ $paises = Paises();
                             <div class="">
                                 <div class="col-sm-12">
                                     <div class="form-group">
-                                        <label for="porque">Cuentanos Porque La Necesitas</label>
+                                        <label for="porque">Cuéntanos Porque La Necesitas</label>
                                         <textarea type="textarea" class="form-control" id="porque" name="porque" placeholder="Porque la necesitas.." rows="3"></textarea>
                                     </div>
                                 </div>
@@ -357,28 +357,28 @@ $paises = Paises();
                                 </div>
 
                                 <div class="col-sm-12">
-                                    <center><h4>Envianos Fotografias o Video</h4></center>
+                                    <center><h4>Envíanos Fotografías o Vídeo</h4></center>
                                 </div>
 
                                 <div class="col-sm-12">
                                     <div class="form-group">
                                         <label for="adjunto1">Adjunto Uno</label>
                                         <input type="file" id="adjunto1" name="adjunto1">
-                                        <p class="help-block">Tamaño maximo de archivo 20 MB</p>
+                                        <p class="help-block">Tamaño máximo de archivo 20 MB</p>
                                     </div>
                                 </div>
                                 <div class="col-sm-12">
                                     <div class="form-group">
                                         <label for="adjunto2">Adjunto Dos</label>
                                         <input type="file" id="adjunto2" name="adjunto2">
-                                        <p class="help-block">Tamaño maximo de archivo 20 MB</p>
+                                        <p class="help-block">Tamaño máximo de archivo 20 MB</p>
                                     </div>
                                 </div>
                                 <div class="col-sm-12">
                                     <div class="form-group">
                                         <label for="adjunto3">Adjunto Tres</label>
                                         <input type="file" id="adjunto3" name="adjunto3">
-                                        <p class="help-block">Tamaño maximo de archivo 20 MB</p>
+                                        <p class="help-block">Tamaño máximo de archivo 20 MB</p>
                                     </div>
                                 </div>
 
@@ -416,7 +416,7 @@ FUNDACIÓN MARKOPTIC A.C. y EL BENEFICIARIO Declaran:
     Mi autorización se refiere a la totalidad de usos que puedan tener las imágenes, o partes de las mismas, en las que aparezco, utilizando los medios técnicos conocidos en la actualidad y los que pudieran desarrollarse en el futuro, y para cualquier aplicación. Todo ello con la única salvedad y limitación de aquellas utilizaciones o aplicaciones que pudieran atentar al derecho al honor en los términos previstos en la Ley Orgánica 1/85, de 5 de Mayo, de Protección Civil al Derecho al Honor, la Intimidad Personal y familiar y a la Propia Imagen. Y al art. 87 de la Ley Federal del Derecho de Autor: “El retrato de una persona sólo puede ser usado o publicado, con su consentimiento expreso, o bien con el de sus representantes o los titulares de los derechos correspondientes. La autorización de usar o publicar el retrato podrá revocarse por quien la otorgó quién, en su caso, responderá por los daños y perjuicios que pudiera ocasionar dicha revocación. 
     Cuando a cambio de una remuneración o aún sin ella, una persona se dejare retratar, se presume que ha otorgado el consentimiento a que se refiere el párrafo anterior y no tendrá derecho a revocarlo, siempre que se utilice en los términos y para los fines pactados. 
     No será necesario el consentimiento a que se refiere este artículo cuando se trate del retrato de una persona que forme parte menor de un conjunto o la imagen sea tomada en un lugar público y con fines informativos o periodísticos. “
-    Mi autorización fija límite de tiempo para su concesión y para la explotación de las imágenes, así como la pista sonora o parte de las mismas, en las que intervengo como EL BENEFICIARIO, por lo que mi autorización se considera concedida por un plazo de tiempo ilimitado para televisión y cualquier medio electrónico, incluyendo internet, y para cualquier medio impreso.  Se considera como inicio del tiempo, una vez que la campaña tanto visual como gráfica, salga por primera vez al aire o este por primera vez en algún  medio impreso, respectivamente. 
+    Mi autorización fija límite de tiempo para su concesión y para la explotación de las imágenes, así como la pista sonora o parte de las mismas, en las que intervengo como EL BENEFICIARIO, por lo que mi autorización se considera concedida por un plazo de tiempo ilimitado para televisión y cualquier medio electrónico, incluyendo Internet, y para cualquier medio impreso.  Se considera como inicio del tiempo, una vez que la campaña tanto visual como gráfica, salga por primera vez al aire o este por primera vez en algún  medio impreso, respectivamente. 
     EL BENEFICIARIO se libera de cualquier cuestión jurídica, a FUNDACIÓN MARKOPTIC A.C., en lo establecido por el Instituto de Transparencia e Información Pública del Estado de Sinaloa, en el presente o futuro, debiendo de sujetarse a lo acordado en este contrato.
     Se considera que es GRATUITO por concepto de pago por la cesión de mis derechos de imágenes y pista sonora, aceptando estar conforme con el citado acuerdo.
 
@@ -438,7 +438,7 @@ Fundación Markoptic A.C. iniciará un proceso de estudio de la solicitud y se c
                                 <div class="col-xs-12">
                                     <div class="form-group">
                                         <label class="checkbox-inline">
-                                        <input type="checkbox" id="checkbox-tyc" value="aceptyc">  Accepto los Terminos y Condiciones.
+                                        <input type="checkbox" id="checkbox-tyc" value="aceptyc">  Acepto los Términos y Condiciones.
                                         </label>
                                     </div>
                                 </div>
@@ -480,6 +480,7 @@ Fundación Markoptic A.C. iniciará un proceso de estudio de la solicitud y se c
 <?php require 'mod/scripts.php';?>
 
 <script type="text/javascript" src="js/funciones.js"></script>
+
     
 </body>
 </html>
