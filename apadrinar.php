33798d85e98e49a06d678cd48515a43022dc1e4e
Jesus Alejandro <alexandrpunk@gmail.com>
2016-03-10 22:08:45 -0700

    arreglado formulario para  registro de padrino

--- apadrinar.php
+++ apadrinar.php
@@ -1,16 +1,7 @@
 <?php
 require 'inc/solicitar.php';
- $title = 'Apadrinar';
-
-    if(isset($_GET['ahijado']) && isset($_GET['padrino']) && $_GET['padrino']!= NULL &&  $_GET['ahijado']!= NULL){
-        $pagina_ahijado= $_GET['ahijado'];
-        $email_padrino= $_GET['padrino'];
-        echo $pagina_ahijado;
-        echo $email_padrino;
-    }
-    else{
-        header('Location:historias');
-    }
+$title = 'Apadrinar';
+$telefono='';
 
 function validar_padrinos($email_padrino){
 
@@ -24,10 +15,7 @@ if (!$con){die("ERROR DE CONEXION CON MYSQL:". mysql_error());}
     $result = $con->query($sql);
     
     if($result->num_rows > 0){
-        echo " se encontro el  email: ";
-        echo $email_padrino;
-        echo '<br>Total results: ' . $result->num_rows;
-
+        
     }else{
         echo " no se encontro el email";
         echo $email_padrino;
@@ -36,7 +24,15 @@ if (!$con){die("ERROR DE CONEXION CON MYSQL:". mysql_error());}
     
 }
 
-validar_padrinos($email_padrino);
+if(isset($_GET['ahijado']) && isset($_GET['padrino']) && $_GET['padrino']!= NULL &&  $_GET['ahijado']!= NULL){
+        $pagina_ahijado= $_GET['ahijado'];
+        $email_padrino= $_GET['padrino'];
+        
+        validar_padrinos($email_padrino);
+    }
+    else{
+        header('Location:historias');
+    }
 
 ;?>
 
@@ -62,9 +58,89 @@ validar_padrinos($email_padrino);
                     ?>
                     
                   <div class="panel panel-default panel-mark animated fadeIn">
-                    <div class="panel-heading panel-heading-mark" id="militar">Solicitud de Recibo Deducible de Impuestos</div>
-                    <div class="panel-body panel-body-mark">    
-                        
+                    <div class="panel-heading panel-heading-mark" id="militar">Solicitud de apadrinamiento</div>
+                    <div class="panel-body panel-body-mark">  
+                     <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>"  method="POST" id="solicitud" class="news" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="nombre" data-toggle="tooltip" data-placement="right" title="Nombre completo de la persona o Razón Social a la cual se hará el recibo">Nombre o Razón Social del padrino</label>
+                                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre completo" required autofocus  maxlength="254" value='<?php echo htmlentities($nombre) ?>'>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="email" data-toggle="tooltip" data-placement="right" title="E-mail al cual se enviara el recibo">Correo Electrónico</label>
+                                <input type="email" name="email" class="form-control" id="email" placeholder="Correo electrónico" required  maxlength="254" value='<?php echo htmlentities($email_padrino) ?>'>
+                            </div>
+                               
+                            <div class="form-group">
+                                <label for="email" data-toggle="tooltip" data-placement="right" title="E-mail al cual se enviara el recibo">Telefono</label>
+                                <input type="tel" name="telefono" class="form-control" id="telefono" placeholder="Numero telefonico de contacto" required  maxlength="254" value='<?php echo htmlentities($telefono) ?>'>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="direccion" data-toggle="tooltip" data-placement="right" title="Dirección Fiscal de facturación a la cual se hará el recibo">Domicilio</label>
+                                <textarea id="direccion" name="direccion" class="form-control" placeholder="Ingrese Calle, Número, Colonia, Código postal, Ciudad, Estado y País" required rows="4"><?php echo htmlentities($direccion) ?></textarea>
+                            </div>
+                            
+                            <div class="checkbox">
+                              <label>
+                               <input name="comprobante" id="comprobante" type="checkbox" value="1">
+                                     <strong>Selecciones esta casilla si desea un recibo deducible de impuestos</strong>
+                              </label>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="rfc" data-toggle="tooltip" data-placement="right" title="Número del Registro Federal de Contribuyentes">R.F.C.</label>
+                                <input type="text" name="rfc" id="rfc" class="form-control" placeholder="Registro Federal del Contribuyente" required  maxlength="13" value='<?php echo htmlentities($rfc) ?>'>
+                            </div>
+                            
+                            <div class="col-md-4 col-sm-4 zero">
+                                <div class="form-group">
+                                <label for="monto" data-toggle="tooltip" data-placement="right" title="Especifique el monto de su donativo en pesos mexicanos, en caso de ser en moneda extranjera especifíquela en el campo de comentarios">Monto del Donativo</label>
+                                <div class="input-group">
+                                  <div class="input-group-addon">$</div>
+                                  <input type="number" name="monto" step="any" min="0" class="form-control" id="monto" placeholder="Monto del donativo" required value='<?php echo htmlentities($monto) ?>'>
+                                </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-4 zero col-sm-4">
+                                <div class="form-group">
+                                 <label for="referencia" data-toggle="tooltip" data-placement="right" title="Numero de referencia del depósito, transferencia bancaria o transferencia por PayPal">No. de Referencia</label>
+                                <input type="number" min="0" name="referencia" class="form-control" id="referencia" placeholder="Numero de Referencia" required value='<?php echo htmlentities($referencia) ?>'>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-4 zero col-sm-4">
+                                <div class="form-group">
+                                <label for="referencia" data-toggle="tooltip" data-placement="right" title="Seleccione el motodo por el cual realizo su donativo">Metodo de pago</label>
+                                <select class="form-control" name="metodo" class="form-control" id="metodo" placeholder="Metdo de pago" required value='<?php echo htmlentities($metodo) ?>'>
+                                    <option value="1">Deposito</option>
+                                    <option value="2">Transferencia bancaria</option>
+                                    <option value="3">Paypal</option>
+                                </select>
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="comentario" data-toggle="tooltip" data-placement="right" title="Si tiene algún comentario o indicación adicional puede hacerlo en este campo">Comentarios</label>
+                                <textarea rows="4" id="comentario" name="comentario" class="form-control" placeholder="Déjanos tu comentario"><?php echo htmlentities($comentario) ?></textarea>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="comprobante" data-toggle="tooltip" data-placement="right" title="Para hacer mas agil la solicitud de su recibo, puede adjuntarnos el comprobante de su deposito escaneado">Comprobante <small>(opcional)</small></label>
+                                <input type="file" name="comprobante">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="captcha" data-toggle="tooltip" data-placement="right" title="Introdusca el codigo de seguridad de letras y numeros que aparecen en la siguiente imagen">Introduzca el codigo de verificacion:</label>
+                                <p><img src="inc/captcha_code_file.php?rand=<?php echo rand(); ?>"
+id="captchaimg" ></p>
+                                <input class="form-control" id="captcha" name="captcha" type="text" style="width:300px;" required autocomplete="off">
+                                <small>¿No puedes leer la imagen? Haz click <a href='javascript: refreshCaptcha();'>aqui</a> Para refrescarla</small> <hr/>
+                            </div>
+
+                              <button type="submit" id="enviar" class="btn btn-lg btn-mark center-block" name="enviar"><strong>Solicitar Recibo</strong></button>
+                        </form>
                     </div>
                 </div>     
             </div>
